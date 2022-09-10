f8ec1b9c4ef1283e4b348e80a08c33a57e54f019
Artem Kolodyazhnyy <a.kolodyazhnyy@mir-dit.ru>
2022-09-10 03:11:12 +0300

    add Stern

---
+++ Stern/src/js/main.js
@@ -0,0 +1,37 @@
+// User JS
+/*var counter = [0,0,0,0,0,0,0,0,0,0];
+var i = 0;
+for (i; i < counter.length; i++) {
+    var istr = String(i);
+    $('#plus' + i).click(function(){
+        counter[i]++;
+        $('#counter' + i).text(counter[i]);
+    });
+    $('#minus' + i).click(function(){
+        if (counter[i] === 0 || counter[i] === '0') {
+            counter[i];
+        } else {
+            counter[i]--;
+        }
+        $('#count' + i).text(counter[i]);
+    });
+}*/
+/*
+var counter0 = 0;
+$("#plus0").click(function(){
+    counter0++;
+    $("#count0").text(counter0);
+});
+$("#minus1").click(function(){
+    if (counter0 === 0 || counter0 === '0') {
+        counter0;
+    } else {
+        counter0--;
+    }
+
+    $("#count0").text(counter0);
+});
+*/
+
+
+
